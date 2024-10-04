216e32553c9f601e149ff7b0ccdb11684f6858e6
21T1 <21T1hoanganh@gmail.com>
2024-10-04 08:44:29 +0700

    .

---
+++ SV21T1020228/SV21T1020228.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SV21T1020228.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
